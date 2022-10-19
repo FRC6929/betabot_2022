616735afe8ec14478928a149c66f64331dfc0046
SmolChicken45 <98850340+SmolChicken45@users.noreply.github.com>
2022-10-19 17:44:08 -0400

    joystick,gamepad,ConDrivetrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -17,7 +19,12 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+  public final Drivetrain m_drivetrain = new Drivetrain();
   // The robot's subsystems and commands are defined here...
+  private final Joystick m_Joystick = new Joystick(0);
+  private final XboxController m_Gamepad = new XboxController(1);
+
+
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
