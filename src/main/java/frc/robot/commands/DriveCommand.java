c7021ff09f2c926cda7c397d74f6308b958124de
SmolChicken45 <98850340+SmolChicken45@users.noreply.github.com>
2022-10-19 18:13:40 -0400

    Drivecommand_v1

---
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+public class DriveCommand extends CommandBase {
+  private final Drivetrain m_drivetrain;
+
+  private final Joystick m_Joystick;
+  private final XboxController m_Gamepad;
+
+  double y;
+  double x;
+
+  /** Creates a new DriveCommand. */
+  public DriveCommand(Joystick j, XboxController g, Drivetrain drivetrain){
+  m_drivetrain = drivetrain;
+  m_Joystick = j;
+  m_Gamepad = g;
+
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    y = -m_Joystick.getY();
+    x = -m_Joystick.getX();
+
+    m_drivetrain.drive(y,x);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
